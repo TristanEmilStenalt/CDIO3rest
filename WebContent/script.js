5fcee06f1bf5e8ca2f7787174f894756de1c9e73
B1gDaddyKane <jenschrj97@gmail.com>
2019-06-10 14:16:41 +0200

    Fjernede gammelt crap

--- WebContent/script.js
+++ WebContent/script.js
@@ -30,25 +30,9 @@ $("#loginButton").click(function () {
  */
 function createUser() {
     event.preventDefault();
-    // var userInfo = $('#userform').not(type=checkbox).serialize();
-    // var userInfo = $('#userform:not([type=checkbox])').serialize();
-    // var userInfo = $('#userform [type=number],[type=text]');
+
     var userInfo = $('#userform').serializeJSON();
     console.log(userInfo);
-
-    // var array_values = [];
-    // $('#userform [type=checkbox]').each( function() { // For hver tjekket checkbox, put værdien i array.
-    //     if( $(this).is(':checked') ) {
-    //         array_values.push( $(this).val() );
-    //     }
-    // });
-    // console.log(array_values);
-    // var roles = array_values.join(",");
-    // console.log(roles);
-
-
-    //var userSamlet = userInfo+"&roles=["+roles+"]";
-    //console.log(userSamlet);
     var userSamlet = userInfo;
 
     $.ajax(
@@ -64,26 +48,6 @@ function createUser() {
         }
     ) //ajax end
 } //createUser end
-
-
-// $("#createUserButton").click(function () {
-//     $.ajax({
-//         url : "/rest/user",
-//         data : $('div#createUserForm').serializeJSON(),
-//         contentType : "application/json",
-//         method : 'POST',
-//         success : function(data){
-//             alert(data);
-//         },
-//         error : function(jqXHR, text, error){
-//             alert(jqXHR.status + text + error);
-//         }
-//     })
-//
-//     $("#adminPage").show();
-//     $("#editPage").hide();
-//
-// });
 
 /**
  * Metode til at sende brugeren tilbage til menuen efter man har
